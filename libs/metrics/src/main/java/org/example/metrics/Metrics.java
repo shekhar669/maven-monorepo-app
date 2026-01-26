3830eccfd271ddd0bf2f0a6ed020003f89e361d3
shekhar669 <68266969+shekhar669@users.noreply.github.com>
2026-01-26 22:07:43 +0530

    feat: metrics lib feature
    feat: metrics lib feature

--- libs/metrics/src/main/java/org/example/metrics/Metrics.java
+++ libs/metrics/src/main/java/org/example/metrics/Metrics.java
@@ -3,6 +3,6 @@ package org.example.metrics;
 public class Metrics {
 
     public void someMethod() {
-        System.out.println("metrics lib");
+        System.out.println("metrics lib feature");
     }
 }
