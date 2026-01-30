969247cca6cd8eecf391c1eb6e2dd8beea5d0d2f
shekhar669 <68266969+shekhar669@users.noreply.github.com>
2026-01-30 12:52:23 +0530

    Update Security.java

--- security/src/main/java/org/example/security/Security.java
+++ security/src/main/java/org/example/security/Security.java
@@ -3,7 +3,7 @@ package org.example.security;
 public class Security {
 
     public void someMethod() {
-         System.out.println("security lib release");
+         System.out.println("security lib release test");
     }
 
     public String getVersion() {
